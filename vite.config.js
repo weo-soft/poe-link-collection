d15977d84629a5deafa4882f0f6c092179fcf912
FloKaemmerer <flo.kaemmerer@gmail.com>
2025-12-04 12:35:24 +0100

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
-  base: '/',
+  base: '/poe-link-collection/',
   publicDir: '../public',
   build: {
     outDir: '../dist',
